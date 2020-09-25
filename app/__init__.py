16173767fc993092a8535a49f4da487cf4d597dd
gracewa <33077294+gracewa@users.noreply.github.com>
2020-09-25 06:55:45 +0300

    initial css styling

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from .config import DevConfig
 from os import environ
+from flask_fontawesome import FontAwesome
+
 
 
 bootstrap = Bootstrap()
@@ -14,7 +16,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
-
+fa = FontAwesome()
 
 photos = UploadSet('photos',IMAGES)
 
@@ -28,6 +30,7 @@ bootstrap.init_app(app)
 db.init_app(app)
 login_manager.init_app(app)
 mail.init_app(app)
+fa.init_app(app)
 
 # configure UploadSet
 configure_uploads(app,photos)
@@ -37,4 +40,4 @@ from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
