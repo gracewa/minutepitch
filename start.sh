ffc71cda76fd941e4b4e150d3398b129c2c15de1
gracewa <33077294+gracewa@users.noreply.github.com>
2020-09-30 09:36:55 +0300

    Update start.sh
    Removed password

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 set MAIL_USERNAME=margaret@agrinovations.com
-set MAIL_PASSWORD=Graciella1.
+set MAIL_PASSWORD=
 set SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:g11111111@localhost/minutepitch'
 
 python manage.py server
